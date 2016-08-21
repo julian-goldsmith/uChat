67b4a70ddff8cb81e8768c45512bda3b6b0f4d24
Julian Goldsmith <shadow0813@gmail.com>
2016-08-21 02:39:11 -0400

    rle working again

--- imgcoder.cpp
+++ imgcoder.cpp
@@ -3,6 +3,7 @@
 #include <memory.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdio.h>
 #include <xmmintrin.h>
 #include "imgcoder.h"
 
@@ -280,10 +281,10 @@ void showRMS(const macroblock_t* blocks, unsigned char* rmsView)
 
 float* rleValue(float data[MB_SIZE][MB_SIZE][4], int* rleSize)
 {
-    float* odata = (float*) calloc(MB_SIZE * MB_SIZE * 4, sizeof(float));    // resize later
+    float* odata = (float*) calloc(MB_SIZE * MB_SIZE * 5, sizeof(float));    // resize later
     int odatacount = 0;
 
-    float counter = 1.0;
+    float counter = 0.0;
 
     float value[3];
     value[0] = data[0][0][0];
@@ -294,7 +295,9 @@ float* rleValue(float data[MB_SIZE][MB_SIZE][4], int* rleSize)
     {
         for(int y = 0; y < MB_SIZE; y++)
         {
-            if(value[0] == data[x][y][0] && value[1] == data[x][y][1] && value[2] == data[x][y][2])
+            if(value[0] == data[x][y][0] &&
+               value[1] == data[x][y][1] &&
+               value[2] == data[x][y][2])
             {
                 counter++;
             }
@@ -313,9 +316,12 @@ float* rleValue(float data[MB_SIZE][MB_SIZE][4], int* rleSize)
         }
     }
 
-    *rleSize = odatacount;
+    odata[odatacount++] = counter;
+    odata[odatacount++] = value[0];
+    odata[odatacount++] = value[1];
+    odata[odatacount++] = value[2];
 
-    assert(odatacount > 0);
+    *rleSize = odatacount;
 
     return (float*) realloc(odata, sizeof(float) * odatacount);
 }
@@ -326,15 +332,13 @@ void unrleValue(float data[MB_SIZE][MB_SIZE][4], float* rleData, int rleSize)
     int x = 0;
     int y = 0;
 
-    //assert(rleSize > 0);
-
     for(int i = 0; i < rleSize; i += 4)
     {
         counter = rleData[i];
 
         while(counter > 0.0)
         {
-            data[y][x][0] = 128.0f;//rleData[i + 1];
+            data[y][x][0] = rleData[i + 1];
             data[y][x][1] = rleData[i + 2];
             data[y][x][2] = rleData[i + 3];
             data[y][x][3] = 0.0f;
@@ -429,7 +433,6 @@ void decodeImage(const unsigned char* prevFrame, compressed_macroblock_t* blocks
 
         unrleBlock(block, data);
         idctBlock(data, pixels);
-        //idctBlock(block->blockDataDCT, pixels);
 
         for(int x = 0; x < MB_SIZE; x++)
         {
